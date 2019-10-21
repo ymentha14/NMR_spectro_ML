41ca1335b578a41a051f095e9c4f4afed9ed73f3
giustogianni <gianni.giusto@epfl.com>
2019-10-21 08:40:54 +0200

    add implementations.py file

---
+++ project1/scripts/implementations.py
@@ -0,0 +1,23 @@
+'''
+Machine Learning (CS-433)
+
+@author: Gianni Giusto, Maxime Epars, Yann Mentha 
+'''
+
+
+# - IMPORTS - #
+import numpy as np
+
+
+# - FUNCTIONS - #
+
+
+# - TRAINING & TESTING - #
+
+
+# - MAIN - #
+
+
+
+
+
