69a419d44fe7062f7dc39c27f180e953180223a3
Macks3110 <55533371+Macks3110@users.noreply.github.com>
2019-10-27 14:45:19 +0100

    boxplot

--- project1/src/vizu.py
+++ project1/src/vizu.py
@@ -5,10 +5,14 @@ def cross_validation_visualization(lambds, metric_tr, metric_te):
     plt.semilogx(lambds, metric_tr, marker=".", color='b', label='train accuracy')
     plt.semilogx(lambds, metric_te, marker=".", color='r', label='test accuracy')
     plt.xlabel("lambda")
-    plt.ylabel("rmse")
-    plt.title("cross validation")
-    plt.legend(loc=2)
+    plt.ylabel("accuracy")
+    plt.legend(loc='upper right')
     plt.grid(True)
     plt.savefig("cross_validation")
+
+def visualize_boxplot_cross_validation(k_data, pos = None):
+    plt.xlabel("lambda")
+    plt.ylabel("accuracy")
+    plt.boxplot(k_data, positions = pos, sym='+')
 
 
